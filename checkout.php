4bb5577a63d500ba2f94b018db30c1d30bc4402b
Suchanan <paul-suchanan@hotmail.com>
2019-07-26 14:11:19 +0700

    My First website commit

---
+++ checkout.php
@@ -0,0 +1,24 @@
+<?php
+
+
+	// define('OMISE_PUBLIC_KEY', 'pkey_test_5goqwp219s7u0ri9rkd');
+	// define('OMISE_SECRET_KEY', 'skey_test_5goqwp21lobatjmgfgq');
+
+	// $charge = OmiseCharge::create(array(
+	// 	'amount' => 1500000,
+	// 	'currency' => 'thb',
+	// 	'card' => $_POST['omiseToken'],
+	// ));
+
+
+	echo '<pre>';
+	print_r($_POST);
+	echo '</pre>';
+
+	echo '<hr>';
+
+	// echo '<pre>';
+	// print_r($charge);
+	// echo '</pre>';
+
+?>
